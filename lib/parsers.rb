caad52503c864ebe11df5af5caddfe168bffa19f
lucerion <alexander.skachko@gmail.com>
2022-12-01 23:17:00 +0300

    Rewrite parser and markdown converter

--- lib/parsers.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative './parsers/base_parser'
-require_relative './parsers/header_parser'
-require_relative './parsers/table_of_contents_parser'
-require_relative './parsers/section_parser'
